074129101174e39008712dff9f671815bfc675c5
Selenewhite <seleneestrada@gmailcom>
2013-07-31 18:48:42 -0500

    Diseño de menú Programas

--- protected/views/telemedellin/seccion.php
+++ protected/views/telemedellin/seccion.php
@@ -14,18 +14,11 @@ cs()->registerScript( 'scroll',
 	CClientScript::POS_READY
 );
 
-cs()->registerScriptFile( bu('js/jquery.isotope/jquery.isotope.min.js'), CClientScript::POS_END );
-	cs()->registerScript( 'isotope', 
-		'$(".mCSB_container").isotope({
-		  itemSelector : "div",
-		  layoutMode : "fitRows"
-		});',
-		CClientScript::POS_READY
-	);
+
 ?>
 <div id="seccion" class="<?php echo $seccion->url->slug; ?>">
 <?php foreach($micrositios as $micrositio):?>
-	<div <?php if($micrositio->destacado) echo 'class="destacado"'?> style="background-image: url('<?php echo bu( $micrositio->miniatura );?>');">
+	<div <?php if($micrositio->destacado) echo 'class="destacado"'?>>
 		<a href="<?php echo bu($micrositio->url->slug); ?>"><h2><?php echo $micrositio->nombre; ?></h2></a>
 	</div>
 <?php endforeach; ?>
