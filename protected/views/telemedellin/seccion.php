29085a50fd4fdbfaf0fa2da683ea7670f0231e2f
Selenewhite <seleneestrada@gmailcom>
2013-08-02 10:15:30 -0500

    Se añadió buscador en "Programas"

--- protected/views/telemedellin/seccion.php
+++ protected/views/telemedellin/seccion.php
@@ -18,12 +18,21 @@ cs()->registerScript( 'scroll',
 ?>
 <div id="seccion" class="<?php echo $seccion->url->slug; ?>">
 <div class="listado">
+<div class="buscadorProgramas">
+ <FORM method=GET action="http://www.google.es/search"> 
+    <fieldset>                   
+        <INPUT TYPE=text id="s" name="q" value="Buscar programa" /> 
+    </fieldset> 
+    </FORM> 
+</div>
+
 <?php foreach($micrositios as $micrositio):?>
 	<div <?php if($micrositio->destacado) echo 'class="destacado"'?>>
 		<a href="<?php echo bu($micrositio->url->slug); ?>"><h2><?php echo $micrositio->nombre; ?></h2></a>
 	</div>
 <?php endforeach; ?>
 </div>
+
 
 <div class="logos">
 <img src="http://localhost/tm/images/logos/altavoz.png" />
