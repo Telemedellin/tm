be138ff52749e021b8952dcef5f7797084e2c6ad
Victor Arias <hugovic1182@gmail.com>
2013-08-02 19:55:00 -0500

    Maquetada la vista seccion

--- protected/views/telemedellin/seccion.php
+++ protected/views/telemedellin/seccion.php
@@ -1,12 +1,10 @@
-
 <div id="contenedorInfo">
 <?php 
 $bc = array();
 $bc[] = ucfirst($seccion->nombre);
 $this->breadcrumbs = $bc;
 $this->pageTitle = $seccion->nombre;
-
-cs()->registerScriptFile( bu('js/jquery.mCustomScrollbar/jquery.mCustomScrollbar.concat.min.js'), CClientScript::POS_END );
+/*cs()->registerScriptFile( bu('js/jquery.mCustomScrollbar/jquery.mCustomScrollbar.concat.min.js'), CClientScript::POS_END );
 cs()->registerScript( 'scroll', 
 	'$(".listado").mCustomScrollbar({
 		scrollButtons:{
@@ -14,29 +12,23 @@ cs()->registerScript( 'scroll',
 		}
 	});',
 	CClientScript::POS_READY
-);
-
-
+);*/
 ?>
 <div class="arrow-before"></div>
 <div id="seccion" class="<?php echo $seccion->url->slug; ?>">
-
+	<div class="buscadorProgramas">
+	 	<form method="GET"> 
+		    <fieldset>                   
+		        <input TYPE="text" id="s" name="q" value="Buscar ..." /> 
+		    </fieldset> 
+	    </form> 
+	</div>
 	<div class="listado">
-		<div class="buscadorProgramas">
-		 	<FORM method="GET"> 
-			    <fieldset>                   
-			        <INPUT TYPE="text" id="s" name="q" value="Buscar programa" /> 
-			    </fieldset> 
-		    </FORM> 
-		</div>
 	<?php foreach($micrositios as $micrositio):?>
 		<div <?php if($micrositio->destacado) echo 'class="destacado"'?>>
 			<a href="<?php echo bu($micrositio->url->slug); ?>"><h2><?php echo $micrositio->nombre; ?></h2></a>
 		</div>
 	<?php endforeach; ?>
-	</div>
-	<div class="logos">
-		<img src="http://localhost/tm/images/logos/altavoz.png" />
 	</div>
 </div>
 </div>
