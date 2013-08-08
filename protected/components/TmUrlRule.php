0234d8c3ec5a4dd6d3c72cdca53920e9cb4549d4
Victor Arias <hugovic1182@gmail.com>
2013-08-08 11:01:07 -0500

    Ajustes iconos, pero queda pendiente

--- protected/components/TmUrlRule.php
+++ protected/components/TmUrlRule.php
@@ -18,15 +18,6 @@ class TmUrlRule extends CBaseUrlRule
     public function parseUrl($manager, $request, $pathInfo, $rawPathInfo)
     {
         
-        /*
-        if ( preg_match('%^([\w-]+)(/([\w-]+))?(/([\w-]+))?%', $pathInfo) )
-        {
-            $slug = Url::model()->findByAttributes( array('slug' => $pathInfo) );
-            if( !$slug ) return false;
-            $_GET['tm'] = $slug;
-            return 'telemedellin/cargar';
-        }
-        */
         if ( preg_match('%^([\w-]+)(/([\w-]+))?(/([\w-]+))?%', $pathInfo) )
         {
             $slug = Url::model()->findByAttributes( array('slug' => $pathInfo) );
