0dabd5fecf6a3b03567ce2295b7ef761b679eedf
Victor Arias <hugovic1182@gmail.com>
2013-09-23 14:05:07 -0500

    arreglado el formato de hora en programacion

--- protected/views/telemedellin/_programacion.php
+++ protected/views/telemedellin/_programacion.php
@@ -11,7 +11,7 @@
 <?php foreach($programas as $programa): ?>
 <div class="programa <?php echo (time() >= $programa->hora_inicio && time() <= $programa->hora_fin)? 'actual':''?>">
 	<a href="<?php echo bu( $programa->micrositio->url->slug ); ?>">
-		<div class="hora"><?php echo date('H:i a', $programa->hora_inicio) ?></div>
+		<div class="hora"><?php echo date('h:i a', $programa->hora_inicio) ?></div>
 		<div class="tit_programa"><?php echo $programa->micrositio->nombre ?></div>
 	</a>
 </div>
