c1d3f4a6acf42617b3b5f109371f3af242f57d53
Victor Arias <hugovic1182@gmail.com>
2014-09-17 16:00:08 -0500

    Cambio la etiqueta Página de blog por Novedad en blog

--- protected/modules/administrador/views/pagina/_paginas.php
+++ protected/modules/administrador/views/pagina/_paginas.php
@@ -6,7 +6,7 @@
   <ul class="dropdown-menu" role="menu">
     <li><?php echo l('Genérica', bu('administrador/pagina/crear/' . $model->id), array('target' => '_blank'))?></li>
     <li><?php echo l('Bloques', bu('administrador/pagina/crear/' . $model->id . '/10'), array('target' => '_blank'))?></li>
-    <li><?php echo l('Página de blog', bu('administrador/pagina/crear/' . $model->id . '/3'), array('target' => '_blank'))?></li>
+    <li><?php echo l('Novedad en blog', bu('administrador/pagina/crear/' . $model->id . '/3'), array('target' => '_blank'))?></li>
     <li><?php echo l('Eventos', bu('administrador/pagina/crear/' . $model->id . '/12'), array('target' => '_blank'))?></li>
     <li><?php echo l('Blog', bu('administrador/pagina/crear/' . $model->id . '/11'), array('target' => '_blank'))?></li>
     <li><?php echo l('Filtro', bu('administrador/pagina/crear/' . $model->id . '/8'), array('target' => '_blank'))?></li>
