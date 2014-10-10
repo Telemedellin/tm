0f9738011238c7573a3580d60f4bd113cc4fc593
Victor Arias <hugovic1182@gmail.com>
2014-10-10 17:24:58 -0500

    Se corrige el link de TM Radio en móviles

--- protected/components/views/programacionw_mobile.php
+++ protected/components/views/programacionw_mobile.php
@@ -2,5 +2,5 @@
 <div id="programacion">
   <h3>Señal en vivo</h3>
   <a class="tv" href="<?php echo bu('senal-en-vivo'); ?>" title="Ver la señal en vivo de Telemedellín"><span>TV</span></a>
-  <a class="tmradio" href="<?php echo bu('radio'); ?>" title="Escuchar la radio de Telemedellín"><span>Radio</span></a>
+  <a class="tmradio" href="<?php echo bu('radio/emisora'); ?>" title="Escuchar la radio de Telemedellín"><span>Radio</span></a>
 </div>
