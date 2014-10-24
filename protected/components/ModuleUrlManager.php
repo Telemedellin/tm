b1477c55de31c6704d62c37560f12bade1b1b6e0
Victor Arias <hugovic1182@gmail.com>
2014-10-24 14:38:06 -0500

    Agregada la caché para las urls de los módulos

--- protected/components/ModuleUrlManager.php
+++ protected/components/ModuleUrlManager.php
@@ -6,12 +6,23 @@ class ModuleUrlManager
 		if($m == null) $m = Yii::app();
 		if(!empty($m->modules))
 		{
+			$cache = $m->getCache();
 			foreach($m->modules as $moduleName => $config)
 			{
-				$module = $m->getModule($moduleName);
-				if(!empty($module->urlRules))
+				$urlRules = false;
+				if($cache)
+					$urlRules = $cache->get('module.urls.'.$moduleName);
+				if($urlRules === false){
+					$urlRules = array();
+					$module = $m->getModule($moduleName);
+					if(isset($module->urlRules))
+						$urlRules = $module->urlRules;
+					if($cache)
+						$cache->set('module.urls.'.$moduleName, $urlRules);
+				}
+				if(!empty($urlRules))
 				{
-					$m->getUrlManager()->addRules($module->urlRules, false);
+					$m->getUrlManager()->addRules($urlRules, false);
 				}
 				if(!empty($module->modules)) $this->collectRules($module);
 			}
