11b682b8c77b80e1cceee365a4a58af52ae268e8
Victor Arias <hugovic1182@gmail.com>
2014-12-05 16:22:48 -0500

    Condición para cuando no hay programa al aire en la barra de programación

--- protected/components/views/programacionw.php
+++ protected/components/views/programacionw.php
@@ -1,10 +1,13 @@
 <?php $programas = $this->getProgramas()?>
-<div id="programacion">
+<?php if($programas['actual']) $actual = true; ?>
+<div id="programacion"<?php if(!$actual) echo ' class="short"'?>>
     <a class="programacion" href="<?php echo bu('/programacion')?>" title="Ver toda la programación de Telemedellín">Toda la programación</a>
+    <?php if($actual): ?>
     <a class="al-aire" href="<?php echo bu('senal-en-vivo'); ?>" title="Ver este programa en la señal en vivo de Telemedellín">
       <span class="estado">Al aire</span> 
-      <span class="programa"><?php if($programas['actual']) echo $programas['actual']->micrositio->nombre ?></span>
+      <span class="programa"><?php echo $programas['actual']->micrositio->nombre ?></span>
     </a>
+    <?php endif; ?>
     <a class="a-continuacion" href="<?php if($programas['siguiente']) echo bu($programas['siguiente']->micrositio->url->slug)?>" title="Ir al micrositio de este programa">
       <span class="estado">A continuación</span> 
       <span class="programa"><?php if($programas['siguiente']) echo $programas['siguiente']->micrositio->nombre ?></span>
