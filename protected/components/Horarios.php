8edcf61d149f0f23836dff2ace65a5d5d791e32f
Victor Arias <hugovic1182@gmail.com>
2015-01-21 16:01:31 -0500

    Refactorización en la visualización de los horarios fijos de los programas

--- protected/components/Horarios.php
+++ protected/components/Horarios.php
@@ -12,10 +12,11 @@ class Horarios{
 
 	public static function horario_programa( $horarios )	
 	{
-		//$dias_semana = array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo');
+		
 		$datos = array();
 		$html = '';
 		foreach( $horarios as $horario ):
+			//Obtengo los datos de cada horario de emisión y lo asigno en el array $datos
 			$datos[] = array(
 				'dia_semana' 		=> $horario->dia_semana,
 				'tipo_emision_id'	=> $horario->tipoEmision->id,
@@ -28,32 +29,24 @@ class Horarios{
 		//Agrupo los datos por tipo de emisión y día
 		usort($datos, "Horarios::ob_tipo_emision_dia");
 
-		for($i = 0; $i < count($datos) ; $i++){
-			switch ($datos[$i]['tipo_emision_id']) {
-				case 1:
-					$en_vivo[] = $datos[$i];
-					break;
-				case 2:
-					$diferido[] = $datos[$i];
-					break;
-				case 3:
-					$reemision[] = $datos[$i];
-					break;
-			}
-		}
-
 		$te = 0;
 		foreach( $datos as $dato )
 		{
+			//Verifico si hay un cambio en el tipo de emisión
+			//Por ejemplo si pasa de ser en vivo a diferido para hacer la aclaración
 			if( $dato['tipo_emision_id'] != $te )
 			{
+				//Si es una reemisión la concateno al texto
 				if( $dato['tipo_emision_id'] == 3 )
 					$html .= $dato['tipo_emision'] . ' ';
 				$te = $dato['tipo_emision_id'];
 			}
+			//Concateno el día de la semana
 			$html .= Horarios::getDiaSemana($dato['dia_semana']) . ' ';
+			//Concateno la hora
 			$html .= ' a las ' . Horarios::hora( $dato['hora_inicio'] ) . ' ';
 		}
+		//Pongo todo el texto en minúsculas y dejo la primera letra de todo el texto en mayúscula
 		$html = ucfirst(strtolower($html));
 		
 		return $html;
@@ -62,10 +55,11 @@ class Horarios{
 	public static function horario_parser( $horarios )	
 	{
 		if( is_null($horarios) ) return false;
-		//$dias_semana = array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo');
+		
 		$datos = array();
 		$html = '';
 		foreach( $horarios as $horario ):
+			//Obtengo los datos de cada horario de emisión y lo asigno en el array $datos
 			$datos[] = array(
 				'dia_semana' 		=> $horario->dia_semana,
 				'tipo_emision_id'	=> $horario->tipoEmision->id,
@@ -79,7 +73,7 @@ class Horarios{
 		usort($datos, "Horarios::ob_tipo_emision_dia");
 
 		for($i = 0; $i < count($datos) ; $i++){
-
+			//Separo los horarios según el tipo de emisión por arrays
 			switch ($datos[$i]['tipo_emision_id']) {
 				case 1:
 					$en_vivo[] = $datos[$i];
@@ -92,21 +86,28 @@ class Horarios{
 					break;
 			}
 		}
+		//Verifico si hay emisiones "en vivo" y genero el html antes que los demás
 		if(isset($en_vivo)){
 			$html .= "En vivo ";			
 			$html .= Horarios::html_emision($en_vivo);
 		}
-
+		//Verifico si hay emisiones "en diferido" y genero el html después de "en vivo" y antes que la reemisión
 		if(isset($diferido)){
 			$html .= Horarios::html_emision($diferido);
 		}	
-		
+		//Por último verifico las reemisiones y genero el html
 		if(isset($reemision)){
+			if( strlen($html) ) $html .= ' y ';
 			$html .= "Reemisión ";
 			$html .= Horarios::html_emision($reemision);
 		}
-		$html = substr($html, 0, -2) . '.';
+		 
+		//Quito la coma (,) final que genera automáticamente Horarios::html_emision() y pongo el punto final
+		//$html = substr($html, 0, -2) . '.';
+		$html .= '.';
+		//Pongo todo el texto en minúsculas y dejo la primera letra de todo el texto en mayúscula
 		$html = ucfirst( strtolower($html) );
+		//Retorno el html
 		return $html;
 	}
 
@@ -114,9 +115,10 @@ class Horarios{
 	{
 		$subitem = null;
 		$html = '';
+		$dias = '';
 		//Ordeno por hora de inicio
 		usort($emision, "Horarios::ob_hora_inicio");
-		//Agrupo por horas diferentes
+		//Agrupo por horas
 		$array_final = array();
 		foreach ($emision as $e) {
 			$hora = $e['hora_inicio'];
@@ -124,40 +126,72 @@ class Horarios{
 		}
 
 		foreach($array_final as $item){
-			
+
+			//Este ciclo permite definir los días de la transmisión a una hora determinada
 			foreach ($item as $subitem) {
 							
+				//Si es el primer día lo agrego limpio al html
 				if ($subitem === reset($item)){
-					$html .= Horarios::getDiaSemana($subitem['dia_semana']) . ' ';
+					$dias .= Horarios::getDiaSemana($subitem['dia_semana']) . ' ';
 				}
 
+				//Si es el segundo item y no hay más después, lo concateno con un "y" al inicio
 				if ($subitem !== reset($item) && count($item) == 2){
 					if($subitem === end($item))
-							$html .= ' y ' . Horarios::getDiaSemana($subitem['dia_semana']) . ' ';
+							$dias .= ' y ' . Horarios::getDiaSemana($subitem['dia_semana']) . ' ';
 				}
+				//Si hay más de dos items
 				if($subitem !== reset($item) && count($item) > 2){
 					$prev = prev($item);
+					//Si el item es contiguo lo concateno con una coma ", " (Por ejemplo, lunes, martes)
 					if($subitem['dia_semana'] === $prev['dia_semana']+1 )
-						$html .= ', ' . Horarios::getDiaSemana($subitem['dia_semana']) . ' ';
+						$dias .= ', ' . Horarios::getDiaSemana($subitem['dia_semana']) . ' ';
+					//Si el item no es contiguo lo separo con una "a" (Por ejemplo, lunes a viernes)
 					else
 						if($subitem === end($item))
-							$html .= ' a ' . Horarios::getDiaSemana($subitem['dia_semana']) . ' ';					
+							$dias .= ' a ' . Horarios::getDiaSemana($subitem['dia_semana']) . ' ';					
 				}
 			}
 
+			//Si no es el primer item, y tampoco el último agrego una coma para separarlo del anterior
+			if( $item !== reset($array_final) && $item !== end($array_final) )
+				$html .=  ', ';
+			//Si es el último ítem y no es el primero agrego un "y" para separarlo del anterior
+			else if( $item === end($array_final) && $item !== reset($array_final)  )
+				$html .=  ' y ';
+
+			$html .= $dias;
+
+			//Verifico si el patrón de los días de emisión se repite, (strpos)
+			//por ejemplo lunes a viernes a las 4pm y lunes a viernes a las 8pm 
+			//en este caso se obviaría el segundo quedando lunes a viernes a las 4pm y a las 8pm.
+			if ($subitem !== reset($item))
+			{
+				$posicion_inicial = strpos($html, $dias);
+				$posicion_final = strrpos($html, $dias);
+				//Busco la primera y la última ocurrencia, si son diferentes es porque hay más de una
+				//en cuyo caso obvio la segunda
+				if( $posicion_inicial !== $posicion_final )
+				{
+					$html = substr_replace( $html, '', $posicion_final );
+				}
+				//También habría que mirar si son varios los que se repiten para poner el y o la coma (,)
+			}
+			$dias = ''; //Limpio el html de días
+
+			//Después de definir los días de la transmisión se pone la hora
 			$html .= ' a la';
+			
+			//Si es entre la 1 y las 2, queda en singular (A la 1:30 pm)
 			if($subitem['hora_inicio'] >= 1300 && $subitem['hora_inicio'] < 1400)
 				$html .= ' ';
+			//Si no es entre la 1 y las 2, queda en plural (A las 2:30 pm)
 			else
 				$html .= 's ';
+			//Concateno la hora de emisión formateada
 			$html .= '<time>' . Horarios::hora( $subitem['hora_inicio'] ) . '</time>';
 
-			if(next($array_final) !== end($array_final) && next($array_final) !== null)
-				$html .=  ' y ';
-			elseif($item !== end($array_final) && count($array_final) > 0)
-				$html .=  ', ';
 		}
-		$html = substr($html, 0, -3) . ', ';
 		return $html;
 	}
 
@@ -165,7 +199,7 @@ class Horarios{
 	{
 		date_default_timezone_set('America/Bogota');
 		setlocale(LC_ALL, 'es_ES.UTF-8');
-		//$dias_semana = array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo');
+		
 		$datos = array();
 		$html = '';
 		foreach( $fechas as $fecha ):
