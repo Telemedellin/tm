3b98fa901678b8548ac523e76c3fc9d3d4106159
Victor Arias <hugovic1182@gmail.com>
2015-02-11 15:34:35 -0500

    Corrección de los textos de la página de error

--- protected/views/telemedellin/error.php
+++ protected/views/telemedellin/error.php
@@ -12,13 +12,13 @@ if($this->beginCache( $code, array('duration' => 21600) )):
 	<div class="contenidoScroll">
 		<h2>Error <?php echo $code ?></h2>
 		<p>¿No encontraste lo que buscabas?</p>
-		<p>Si querés pegate una pasada por nuestros <?php echo l('programas', bu('programas')) ?> o nuestra <?php echo l('parrilla de programación', bu('programacion')) ?>.</p>
-		<p>Si todavía no encontrás lo que buscas, podés preguntarnos en nuestras redes sociales:</p>
+		<p>Si quieres da una pasada por nuestros <?php echo l('programas', bu('programas')) ?> o nuestra <?php echo l('parrilla de programación', bu('programacion')) ?>.</p>
+		<p>Si todavía no encuentras lo que buscas, puedes preguntarnos en nuestras redes sociales:</p>
 		<ul>
 			<li><?php echo l('Twitter', CHtml::normalizeUrl('http://twitter.com/telemedellin'), array('target' => '_blank', 'rel' => 'nofollow') ) ?></li>
 			<li><?php echo l('Facebook', CHtml::normalizeUrl('http://facebook.com/telemedellintv'), array('target' => '_blank', 'rel' => 'nofollow')) ?></li>
 		</ul>
-		<p>O también podés <?php echo l('escribirnos', bu('telemedellin/utilidades/escribenos')) ?></p>
+		<p>O también puedes <?php echo l('escribirnos', bu('telemedellin/utilidades/escribenos')) ?></p>
 	</div>
 </div>
 <?php $this->endCache(); endif; ?>
